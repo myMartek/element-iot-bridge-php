6a5f17105dd1979c0a069897c940fb4cc2378acc
Lucas Koch <l.koch@mainova.de>
2021-09-14 13:16:43 +0200

    Added first implementation of getAll

--- src/ElementIotBridge.php
+++ src/ElementIotBridge.php
@@ -6,7 +6,7 @@ use GuzzleHttp\Client;
 class ElementIoTBridge {
   public static function request(String $url, Array $data = null, String $method = 'GET', Array $options = []) {
     $options = array_merge(['verify' => false], $options);
-
+    
     $client = new Client($options);
 
     if ($data != Null) {
@@ -47,7 +47,39 @@ class ElementIoTBridge {
   }
 
   public static function getAll(String $url) {
-    return [];
+    $result = [];
+    $res = Null;
+    $tryagain = false;
+    $retrieveafter = '';
+    $client = new Client();
+
+    do {
+      try {
+        $response = $client->request($method, $url, ['json' => $data]);
+
+        return json_decode($response->getBody()->getContents(), false);
+      } catch (\GuzzleHttp\Exception\ClientException $e) {
+        if ($e->getCode() == 429 && $e->hasResponse()) {
+          $response = $e->getResponse();
+          
+          if ($response->hasHeader('x-ratelimit-reset')) {
+            usleep(intval($response->getHeader('x-ratelimit-reset')[0]) + 1);
+
+          }
+        } else {
+          break;
+        }
+      } catch (\Exception $e) {
+        break;
+      }
+      $res = $client->request('GET', $url);
+
+      $retrieveafter = $res->data->retrieve_after_id;
+      array_push($result, $res);
+
+    } while (count($res) == 100 || $tryagain);
+
+    return $result;
   }
 
   public static function getStream(String $url) {
