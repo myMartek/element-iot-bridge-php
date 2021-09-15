e9790e020177861374f160a171e94e30983ba3d9
Lucas Koch <l.koch@mainova.de>
2021-09-15 15:35:25 +0200

    Updated autoloading

--- tests/GetStreamTest.php
+++ tests/GetStreamTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../src/ElementIotBridge.php';
 
 use PHPUnit\Framework\TestCase;
 use Mainova\ElementIoTBridge;
